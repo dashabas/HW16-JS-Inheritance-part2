ac695a69d757d2e4966ef956ccf8d30425f3e60e
dashabasya <dashabasya29@gmail.com>
2022-05-26 18:54:09 +0200

    add HW16-JS-Inheritance-part2

---
+++ src/script.js
@@ -0,0 +1,66 @@
+class Clock {
+    constructor(element) {
+        this.element = element;
+        this.fullFormat = true;
+        this.shortFormat = false;
+        this.element.addEventListener('click', () => {
+            this.changeFormat();
+        });
+    }
+
+    render() {
+        if (this.fullFormat) {
+            this.element.innerHTML = new Date().toTimeString().split(' ')[0];
+        } else {
+            this.element.innerHTML = new Date().toTimeString().split(' ')[0].substring(0,5);
+        }
+    }
+
+    changeFormat() {
+        this.fullFormat = !this.fullFormat;
+        this.shortFormat = !this.shortFormat;
+    }
+}
+
+const clock = new Clock(document.querySelector('.clock'));
+setInterval(() => clock.render(), 1000);
+
+
+class ClockFullFormat extends Clock{
+    constructor(element) {
+        super(element);
+    }
+
+    render() {
+        this.element.innerHTML = new Date().toTimeString().split(' ')[0];
+
+        if (!this.fullFormat) {
+            this.element.innerHTML = new Date().toTimeString().split(' ')[0].substring(0,5);
+        }
+    }
+}
+
+const fullClock = new ClockFullFormat(document.querySelector('.clock-full-format'));
+setInterval(() => fullClock.render(), 1000);
+
+
+class ClockShortFormat extends Clock{
+    constructor(element) {
+        super(element);
+    }
+
+    render() {
+        if (this.shortFormat) {
+            this.element.innerHTML = new Date().toTimeString().split(' ')[0];
+        } else {
+            this.element.innerHTML = new Date().toTimeString().split(' ')[0].substring(0,5);
+        }
+    }
+}
+
+const shortClock = new ClockShortFormat(document.querySelector('.clock-short-format'));
+setInterval(() => shortClock.render(), 1000);
+
+console.log(clock);
+console.log(fullClock);
+console.log(shortClock);
